6bf182306f47bbedc00a48be3b4da0f47a42bf70
Barry Frost <public@barryfrost.com>
2021-05-17 20:54:57 +0100

    Verify inbound webmentions

--- src/events/verify/index.js
+++ src/events/verify/index.js
@@ -1,5 +1,48 @@
-// learn more about event functions here: https://arc.codes/primitives/events
-exports.handler = async function subscribe (event) {
-  console.log(JSON.stringify(event, null, 2))
-  return
+const fetch = require('node-fetch')
+const arc = require('@architect/functions')
+const status = require('@architect/shared/status')
+
+const validContentTypes = [
+  'text/html',
+  'application/json',
+  'text/plain'
+]
+
+async function handler (event) {
+  const { id } = event
+
+  const data = await arc.tables()
+  const webmention = await data.webmentions.get({ id })
+  if (!webmention) {
+    status.error(id, 'Webmention could not be found')
+    return
+  }
+
+  const response = await fetch(webmention.source, {
+    method: 'get',
+    size: 1024 * 1024
+  })
+  if (!response.ok) {
+    status.error(id, 'Source could not be fetched')
+    return
+  }
+  const contentType = response.headers.get('content-type')
+  if (!validContentTypes.includes(contentType)) {
+    status.error(id, 'Source Content-Type was invalid')
+    return
+  }
+  const text = await response.text()
+  if (!text.match(webmention.target)) {
+    status.error(id, 'Source does not include a link to target')
+    return
+  }
+
+  await arc.events.publish({
+    name: 'parse',
+    payload: { id }
+  })
+
+  await status.log(id, 'Source links to target')
 }
+
+exports.handler = arc.events.subscribe(handler)
