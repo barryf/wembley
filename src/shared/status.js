ce9a7492c66c2491f997f242d3ce5ded7b094fa5
Barry Frost <public@barryfrost.com>
2021-05-17 20:54:15 +0100

    Support log and error types of statuses

--- src/shared/status.js
+++ src/shared/status.js
@@ -1,15 +1,25 @@
 const arc = require('@architect/functions')
 
-async function create ({ webmentionId, message }) {
+async function create (webmentionId, message, type) {
   const data = await arc.tables()
   const id = Math.random().toString(36).substring(2)
   await data.statuses.put({
     id,
     webmention_id: webmentionId,
     created_at: new Date().toISOString(),
+    type,
     message
   })
-  return id
 }
 
-module.exports = { create }
+async function log (webmentionId, message) {
+  console.log(`${webmentionId} ${message}`)
+  await create(webmentionId, message, 'log')
+}
+
+async function error (webmentionId, message) {
+  console.error(`${webmentionId} ${message}`)
+  await create(webmentionId, message, 'error')
+}
+
+module.exports = { log, error }
