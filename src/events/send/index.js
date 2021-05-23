f99f92a883b7bd8950c17ca39fb72065f95a00d4
Barry Frost <public@barryfrost.com>
2021-05-23 21:17:21 +0100

    Add send event

--- src/events/send/index.js
+++ src/events/send/index.js
@@ -1,5 +1,42 @@
-// learn more about event functions here: https://arc.codes/primitives/events
-exports.handler = async function subscribe (event) {
-  console.log(JSON.stringify(event, null, 2))
-  return
+const fetch = require('node-fetch')
+const arc = require('@architect/functions')
+const status = require('@architect/shared/status')
+const isValidURL = require('@architect/shared/valid-url')
+
+async function handler (event) {
+  const { id } = event
+
+  const data = await arc.tables()
+  const webmention = await data.webmentions.get({ id })
+  if (!webmention) {
+    status.error(id, 'Webmention could not be found')
+  }
+
+  const url = process.env.WEBHOOK_URL
+  if (!(url)) {
+    status.error(id, 'WEBHOOK_URL has not been defined in ENV')
+    return
+  }
+  if (!isValidURL(url)) {
+    status.error(id, 'WEBHOOK_URL is not a valid URL')
+    return
+  }
+
+  const response = await fetch(url, {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(webmention.post)
+  })
+
+  console.log('webhook', JSON.stringify(webmention.post, null, 2))
+
+  if (response.ok) {
+    status.log('Webhook post was successful')
+  } else {
+    status.error('Webhook post failed')
+  }
+
+  return response.ok
 }
+
+exports.handler = arc.events.subscribe(handler)
